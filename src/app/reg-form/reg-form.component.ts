3c9b521c67f9453c01f0fc08cf24a7dbe7fbbe79
jacksonsuthip <jackantoaj@gmail.com>
2022-12-06 00:15:06 +0530

    :hammer: form setup

--- src/app/reg-form/reg-form.component.ts
+++ src/app/reg-form/reg-form.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./reg-form.component.css']
 })
 export class RegFormComponent {
-
+  uName = ''
+  city = ''
+  address = ''
+  gen = ''
+  onChangeName(event) {
+    this.uName = event.target.value
+  }
+  onClickCity(event) {
+    this.city = event.target.value
+  }
+  onChangeAddress(event) {
+    this.address = event.target.value
+  }
+  onClickGen(event) {
+    if (event.target.id === 'genM') {
+      this.gen = 'male'
+    } else if (event.target.id === 'genF') {
+      this.gen = 'female'
+    } else {
+      this.gen = 'other'
+    }
+  }
 }
