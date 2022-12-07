2d4cedf5ed696cb6f5cb5c1dad12a08e570297d4
Anto Jackson Suthip J <antoj@amadisglobal.com>
2022-12-07 18:02:07 +0530

    feat: :hammer: dynamic style and directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html',
-    //   styleUrls: ['./app.component.css']
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      .styleClass {
+        background-color: #adadad;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
+  name = 'my-angular-app';
+  btnDisable = false;
+  status = false;
+  available = 'offline';
+
+  constructor() {
+    this.status = Math.random() > 0.5 ? true : false;
+    this.available = this.status ? 'offline' : 'online';
+  }
+
+  onClickButton() {
+    this.status = !this.status;
+    this.available = this.status ? 'offline' : 'online';
+    console.log(this.status);
+  }
+  getColor() {
+    return this.status === true ? 'red' : 'green';
+  }
 }
